3c1d6d33df9d7a108bf02f29c2611be2c61cafd4
immike1112 <immike1112@gmail.com>
2016-08-07 13:39:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+// Loading the index file . html displayed to the client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Loading socket.io
+var io = require('socket.io').listen(server);
+
+// When a client connects, we note it in the console
+io.sockets.on('connection', function (socket) {
+    console.log('A client is connected!');
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', 'You are connected!');
+
+    // When the server receives a “message” type signal from the client   
+    socket.on('message', function (message) {
+        console.log('A client is speaking to me! They’re saying: ' + message);
+    }); 
+});
+
+
+server.listen(8080);
